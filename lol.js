963e7e5b01f2cf8d0c6741997ac6c9f95cb34f77
test0071030 <e14051033@gs.ncku.edu.tw>
2019-03-12 15:07:37 +0800

    www`

---
+++ lol.js
@@ -0,0 +1,7 @@
+<script language="javascript"> 
+      function changesize(){ 
+        window.resizeTo(500,833); //指定預開啟的寬度與高度
+      } 
+      window.onload=changesize; 
+      window.onresize=changesize; 
+    </script>
