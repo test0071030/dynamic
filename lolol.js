9828eefde4e722d214abcdcd32bdd1eff36fc7e3
test0071030 <e14051033@gs.ncku.edu.tw>
2019-03-18 12:48:48 +0800

    Finally Done

--- lolol.js
+++ lolol.js
@@ -1,38 +1,79 @@
 $(document).ready(function() { 
 
+
   $(".word").hide();
   $(".word").each(function(i) {
     if (this.complete) {
-      $("#w1").fadeIn(1800);
-      $("#w2").delay(1800).fadeIn(1800);
-      $("#w3").delay(3600).fadeIn(1000);
-      $("#w4").delay(4600).fadeIn(1000);
+      $("#w1").fadeIn(1000);
+      $("#w2").delay(1000).fadeIn(1500);
+      $("#w3").delay(2500).fadeIn(1500);
+      $("#w4").delay(4000).fadeIn(1500);
+
     } else {
       $(this).load(function() {
-        $("#w1").fadeIn(1000);
-        $("#w2").delay(1000).fadeIn(1000);
-        $("#w3").delay(2000).fadeIn(1000);
-        $("#w4").delay(3000).fadeIn(1000);
+      $("#w1").fadeIn(1000);
+      $("#w2").delay(1000).fadeIn(1500);
+      $("#w3").delay(2500).fadeIn(1500);
+      $("#w4").delay(4000).fadeIn(1500);
+
       });
     }
   });
 
   $("#plate")
     .animate({
-    left: '500px',bottom: '15px'},1000)
-    .delay(800)
-    .animate({
-    left: '-170px',bottom: '90px'},1500)
-
-    ;
+      bottom:'-13px',
+      left: '610px'},1000)
+       ;
 
   $("#soup")
     .animate({
-    left: '800px',bottom: '30px'},1800)
-    .animate({
-    left: '-200px',bottom: '120px'},1800)
+      bottom: '-3px',
+      left: '520px'},1000)
+       ;
 
-    ;
+  $("#plate1")
+    .delay(1000)
+    .animate({
+      bottom:'87px',
+      left: '-220px'},1500)
+       ;
+
+  $("#soup1")
+    .delay(1000)
+    .animate({
+      bottom: '95px',
+      left: '-100px'},1500)
+       ;
+
+  $("#plate2")
+    .delay(2500)
+    .animate({
+      bottom:'200px',
+      left: '610px'},1500)
+       ;
+
+  $("#soup2")
+    .delay(2500)
+    .animate({
+      bottom: '210px',
+      left: '520px'},1500)
+       ;
+
+  $("#plate3")
+    .delay(4000)
+    .animate({
+      bottom:'310px',
+      left: '30px'},1500)
+       ;
+
+  $("#soup3")
+    .delay(4000)
+    .animate({
+      bottom: '310px',
+      left: '150px'},1500)
+       ;
+
 
   $("#b").click(function(){
         $("#knife").fadeIn(300);
